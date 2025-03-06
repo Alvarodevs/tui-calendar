4b990a245daf50ec73d79beac4fc26dd795b0889
Alvaro Garzon <alvaro.develops@gmail.com>
2025-03-06 02:12:03 +0100

    add package

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MyCalendar from "./components/calendar/Calendar";
+
+function App() {
+  return (
+    <div>
+      <h1>Mi Calendario</h1>
+      <MyCalendar />
+    </div>
+  );
+}
+
+export default App;
